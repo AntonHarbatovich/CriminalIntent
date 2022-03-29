b22caa51a62075680d1a213ff72a81a30969e6a2
Anton Harbatovich <a.harbatovich@gmail.com>
2022-03-29 19:14:08 +0300

    passed character 11

--- app/src/main/java/com/example/criminalintent/Crime.kt
+++ app/src/main/java/com/example/criminalintent/Crime.kt
@@ -1,10 +1,13 @@
 package com.example.criminalintent
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 import java.util.*
 
+@Entity
 data class Crime(
-    val id: UUID = UUID.randomUUID(),
+    @PrimaryKey val id: UUID = UUID.randomUUID(),
     var title: String = "",
     val date: Date = Date(),
-    var isSolved:Boolean = false
+    var isSolved: Boolean = false
 )
